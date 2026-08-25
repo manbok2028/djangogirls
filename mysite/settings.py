d615a2991bec0ae4286e511719b501378516cd1a
김만복 <myjoy21c@naver.com>
2026-08-25 17:42:24 +0900

    Add allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sm23vlz%=-&en9(eqbn-_0rnu($^xx$&zy-udyme2_rh-3i##c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['manbokkim.pythonanywhere.com']
 
 
 # Application definition
