737c60a537ba02309eb0b4ae9fb6d56a6f44e182
김만복 <myjoy21c@naver.com>
2026-08-27 14:40:56 +0900

    Add urls, views, and template for blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sm23vlz%=-&en9(eqbn-_0rnu($^xx$&zy-udyme2_rh-3i##c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['manbokkim.pythonanywhere.com']
+ALLOWED_HOSTS = ['manbokkim.pythonanywhere.com','127.0.0.1', 'localhost']
 
 
 # Application definition
